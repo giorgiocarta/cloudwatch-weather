7db8e0cb9d718af8b3ff57b46503fc6eb97cbc44
donedeal-giorgio <giorgio@donedeal.ie>
2020-05-04 11:26:14 +0100

    unit tests

--- aws_weather/tests/unit/test_handler.py
+++ aws_weather/tests/unit/test_handler.py
@@ -1,74 +1,30 @@
 import json
+import mock
+from unittest import TestCase
 
-import pytest
+from mock import patch
 
-from aws-weather import app
+# monkey patching before the decorator is loaded
+patch('aws_embedded_metrics.metric_scope', lambda x: x).start()
 
-from
-
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
-
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+from aws_weather.get_met_data import app
 
 
-def test_lambda_handler(apigw_event, mocker):
+class TestUUT(TestCase):
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
+    def test_response_lambda_handler(self):
+        from aws_weather.get_met_data import app
 
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+        ret = app.lambda_handler({}, "", mock.MagicMock())
+        data = json.loads(ret["body"])
+
+        assert ret["statusCode"] == 200
+        assert "message" in ret["body"]
+        assert data["message"] == "OK"
+
+    def test_metrics_calls(self):
+        context = {}
+        number_of_metrics = len(app.met_metrics)
+        mymock = mock.Mock()
+        app.lambda_handler(event={}, context=context, metrics=mymock)
+        assert mymock.put_metric.call_count == number_of_metrics
